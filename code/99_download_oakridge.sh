d99a4fa95372f1b8b1faa5c0380d11c92d1e77cc
David Lafferty <davidcl2@illinois.edu>
2023-11-24 14:00:18 -0500

    more precalibration runs

--- code/99_download_oakridge.sh
+++ code/99_download_oakridge.sh
@@ -13,7 +13,7 @@ echo "Job started on `hostname` at `date`"
 # Go to the correct place
 cd $PBS_O_WORKDIR
 
-export STORAGE_DIR=/gpfs/group/kaf26/default/public/OakRidgeCMIP6
+export STORAGE_DIR=/storage/group/pches/default/public/OakRidgeCMIP6
 
 wget --no-check-certificate -i ../utils/oakridge_cmip6_temp_precip_all.txt -P $STORAGE_DIR
 
